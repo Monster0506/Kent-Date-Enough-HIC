805fbb9a7719ed12617878cfc3f259cda50772e8
TJ Raklovits <tjraklovits@gmail.com>
2026-04-08 06:56:14 -0400

    Static file serving

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def main():
-    print("Hello from kent-date-enough-hic!")
+from kindling import Application
+
+app = Application(template_dir="templates")
+app.static("/static", "static")
 
 
 if __name__ == "__main__":
-    main()
+    app.run(host="127.0.0.1", port=8000)
