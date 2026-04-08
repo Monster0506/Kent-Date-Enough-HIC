65d2d0537224463a1e659e4e6da00e93b37294a7
Monster0506 <tjraklovits@gmail.com>
2026-04-08 07:21:36 -0400

    landing page

--- main.py
+++ main.py
@@ -4,5 +4,10 @@ app = Application(template_dir="templates")
 app.static("/static", "static")
 
 
+@app.get("/")
+def landing(req):
+    return app.render("landing.html")
+
+
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=8000)
